1488f1a2aa93a54f3df7719ee817531359a1a559
ShamimIslam <shamim.islam@gmail.com>
2022-04-04 21:15:23 -0400

    2 files modified

--- src/app/contact-info/contact-info.component.ts
+++ src/app/contact-info/contact-info.component.ts
@@ -3,21 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact-info',
   templateUrl: './contact-info.component.html',
-  styleUrls: ['./contact-info.component.css']
+  styleUrls: ['./contact-info.component.css'],
 })
 export class ContactInfoComponent implements OnInit {
-
-  constructor() { }
-  data  = {
-    name:{value:'',type:'text'},
-    address:{value:'',type:'text'},
-    phone:{value:'',type:'phone'},
-    city:{value:'',type:'text'},
-    state:{value:'',type:'text'},
-    zip:{value:'',type:'text'},
-    dob:{value:'',type:'date'},
-  }
-  ngOnInit() {
-  }
-
+  constructor() {}
+  fields = 'name,address,phone,city,state,zip,dob'.split(',');
+  data = {
+    name: { value: '', type: 'text' },
+    address: { value: '', type: 'text' },
+    phone: { value: '', type: 'phone' },
+    city: { value: '', type: 'text' },
+    state: { value: '', type: 'text' },
+    zip: { value: '', type: 'text' },
+    dob: { value: '', type: 'date' },
+  };
+  ngOnInit() {}
 }
